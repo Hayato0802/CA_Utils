80fce6be7daac30b19a2ac7f5650db4e35bdb05e
Hayato0802 <nishida.hayato@blueship.co.jp>
2025-08-04 13:08:21 +0900

    fix

--- scripts/update.ps1
+++ scripts/update.ps1
@@ -24,16 +24,13 @@ if (Test-Path "$installDir\manifest.json") {
     $currentVersion = (Get-Content "$installDir\manifest.json" | ConvertFrom-Json).version
     if ($currentVersion -eq $latestVersion) {
         Write-Host "既に最新版がDLされています (v$currentVersion)" -ForegroundColor Green
-        Start-Process (Resolve-Path $installDir)
         exit 1
     }
     Write-Host "更新中... from v$currentVersion to v$latestVersion" -ForegroundColor Cyan
-    Start-Process (Resolve-Path $installDir)
 } else {
+    $currentVersion = "未インストール"
     Write-Host "Installing v$latestVersion" -ForegroundColor Cyan
-    Start-Process (Resolve-Path $installDir)
     Write-Host "$installDir"
-    exit 1
 }
 
 # Download and install
@@ -45,8 +42,18 @@ try {
     Expand-Archive -Path $tempFile -DestinationPath $installDir -Force
     Remove-Item $tempFile
     
-    Write-Host "Updated successfully!" -ForegroundColor Green
-    Write-Host "Reload the extension in Chrome" -ForegroundColor Yellow
+    # 更新の場合
+    if( $currentVersion -ne "未インストール" ) {
+        Write-Host "Updated successfully!" -ForegroundColor Green
+        Write-Host "拡張機能画面(chrome://extensions/)からCo-Assign Utilsをreloadしてください！" -ForegroundColor Red
+    } else {
+        # 初回インストールの場合
+        Write-Host "installed successfully!" -ForegroundColor Green
+        Write-Host "別ウィンドウで開かれたフォルダ（$installDir）にconfig.jsを追加したのち、" -ForegroundColor Red
+        Write-Host "拡張機能画面(chrome://extensions/)にて「パッケージ化されていない拡張機能を読み込む」から上記のフォルダを追加してください！" -ForegroundColor Red
+        Start-Process (Resolve-Path $installDir)
+    }
+    exit 1
 } catch {
     Write-Host "Update failed: $_" -ForegroundColor Red
     exit 1
